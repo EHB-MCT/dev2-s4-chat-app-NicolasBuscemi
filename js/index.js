ee6719c081fbce1991d3aa79ed9d29949ee744a8
NicolasBuscemi <91195492+NicolasBuscemi@users.noreply.github.com>
2023-03-15 12:13:30 +0100

    second commit

--- js/index.js
+++ js/index.js
@@ -3,17 +3,45 @@
 const chat = {
     author: "yourName",
     init() {
+        this.fetchMessages();
     },
-    sendMessage() {
-    },
+    sendMessage() {},
     fetchMessages() {
         fetch('https://dev2chat.onrender.com/messages')
-        .then(response => response.json())
-        .then(data => console.log(data));
-        
+            .then((response) => {
+                return response.json();
+
+            })
+            .then((data) => {
+                console.log(data);
+                data.sort((a, b) => {
+                    if (a.id < b.id) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                });
+                data.forEach((msg) => {
+                    let htmlString = `
+                    <div class="header">
+                    <span class="author">${msg.author}</span>
+                    <span class="time">00:00</span>
+                    </div>
+                    <p>
+                    ${msg.message}
+                    </p>
+                    </div>`
+                    document.querySelector("#messageContainer").insertAdjacentHTML("beforeend", htmlString)
+                });
+
+
+
+            });
     },
     renderMessage(message) {
+
     }
+
 
 }
 
